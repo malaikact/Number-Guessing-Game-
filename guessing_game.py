ae0751b30c8d8917e668bca3a232e6d5db3ad657
malaikact <68908421+malaikact@users.noreply.github.com>
2021-03-25 19:52:03 -0400

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,8 +1,8 @@
 import random          
 def start_game():
-    score = 0 
+    scores = [] 
     name = input("Hi there \U0001F44B, welcome to the Number Guessing Game! \U0001F3AE What is your name?  ") 
-    print("The highscore is {}, let's see if you can beat it.".format(score))
+    print("The highscore is {}, let's see if you can beat it.".format(scores))
     print("Let's get started {}!".format(name))
     
     number = random.randint(1,10)
@@ -11,31 +11,29 @@ def start_game():
     attempts = 1
 
     while guess != number:
-        if guess > 10 or guess < 0:
+        if guess > 10 or guess < 1:
             print("Oops! That number is not within the range. Try again")
             guess = guess_again(guess)
         elif guess > number:
             print("It's lower")
-            attemps +=1
+            attempts +=1
             guess = guess_again(guess)
         else:
             print("It's higher")
             attempts +=1
             guess = guess_again(guess)
 
-    if attempts < score: 
-        score = attempts
-    elif score == 0:
-        score += attempts
+    scores.append(attempts)    
+    highscore = min(scores) 
 
     print("Got it!")
-    print("Congratulations {}, \U0001F389 you have guessed the correct number! It only took you {} attempts and your score is now {}. Way to go smarty pants! The game is now over.".format(name, attempts, score))        
+    print("Congratulations {}, \U0001F389 you have guessed the correct number! It only took you {} attempts and your highscore is now {}. Way to go smarty pants! The game is now over.".format(name, attempts, highscore))        
     play_again = input("Would you like to play again? (Yes/No)")
     if play_again.lower() == "yes":
         start_game()
     elif play_again.lower() == "no":
         print("Thanks for playing with us {}. See you next time!".format(name))
-   
+  
 
 def guess_again(guess):
     while True: 
