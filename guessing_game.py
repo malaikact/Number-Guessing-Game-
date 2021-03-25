2c40aba68a617a654916125812b058fcae96b3a8
malaikact <68908421+malaikact@users.noreply.github.com>
2021-03-24 21:53:31 -0400

    Update guessing_game.py
    fixing errors

--- guessing_game.py
+++ guessing_game.py
@@ -1,33 +1,50 @@
-import random
-
-            
+import random          
 def start_game():
+    score = 0 
     name = input("Hi there \U0001F44B, welcome to the Number Guessing Game! \U0001F3AE What is your name?  ") 
+    print("The highscore is {}, let's see if you can beat it.".format(score))
     print("Let's get started {}!".format(name))
     
     number = random.randint(1,10)
     
+    guess = guess_again(0)
+    attempts = 1
+
+    while guess != number:
+        if guess > 10 or guess < 0:
+            print("Oops! That number is not within the range. Try again")
+            attempts +=1
+            guess = guess_again(guess)
+        elif guess > number:
+            print("It's lower")
+            attemps +=1
+            guess = guess_again(guess)
+        else:
+            print("It's higher")
+            guess = guess_again(guess)
+
+    if attempts < score: 
+        score = attempts
+    elif score == 0:
+        score += attempts
+
+    print("Got it!")
+    print("Congratulations {}, you have guessed the correct number! It only took you {} attempts and your score is now {}. Way to go smarty pants! The game is now over.".format(name, attempts, score))        
+    play_again = input("Would you like to play again? (Yes/No)")
+    if play_again.lower() == "yes":
+        start_game()
+    elif play_again.lower() == "no":
+        print("Thanks for playing with us {}. See you next time!".format(name))
+   
+
+def guess_again(guess):
     while True: 
         try: 
-            guess = input("Pick a random number from 1 to 10:  ")
-            guess = int(guess) 
+            guess = int(input("Pick a random number from 1 to 10:  "))
             break
         except ValueError:
             print("Oops! You need to type in the numerical value of any number from 1 to 10. Try again")
-            
-        
-    
-    while guess != number:
-        if guess > number:
-            print("It's lower")
-            guess = input("Guess again  ")
-            guess = int(guess)
-        else:
-            print("It's higher")
-            guess = input("Guess again  ")
-            guess = int(guess)
-        
-    print("Got it")
-    print("Congratulations {}, you have guessed the correct number! Way to go smarty pants! The game is now over.".format(name))        
-   
+            continue
+    return guess 
+
 start_game()
